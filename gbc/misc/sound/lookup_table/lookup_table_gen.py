9fec1030c2ae9d4635125f670a666d7459b5cae8
dcampbel <dncswim76@gmail.com>
2014-11-02 19:47:49 -0500

    updated lookup tables to calculate number of clock cycles

--- gbc/misc/sound/lookup_table/lookup_table_gen.py
+++ gbc/misc/sound/lookup_table/lookup_table_gen.py
@@ -3,20 +3,23 @@ bram_sim_file = open("lookup_table.dat", "w")
 
 sim_header = "@0000/n"
 synth_header = "memory_initialization_radix=16;\nmemory_initialization_vector=\n"
+CLOCKRATE = 33000000 #running on a 33 Mhz Clock
 
 bram_file.write(synth_header)
 bram_sim_file.write(sim_header)
 
 for i in range(0,2**11 - 1):
-    number = 2**17/(2**11 - i)
-    hex_string = hex(int(number))    #convert to a string
+    frequency = 2**17/(2**11 - i)
+    clocks_in_period = CLOCKRATE/frequency
+    hex_string = hex(int(clocks_in_period))    #convert to a string
     hex_string = hex_string[2:] #remove the "0x"
     bram_file.write(hex_string + ",\n")
     bram_sim_file.write(hex_string + "\n")
 
 #the last value
-number = 2**17
-hex_string = hex(number)    #convert to a string
+frequency = 2**17
+clocks_in_period = CLOCKRATE/frequency
+hex_string = hex(int(clocks_in_period))    #convert to a string
 hex_string = hex_string[2:] #remove the "0x"
 bram_file.write(hex_string)
 bram_sim_file.write(hex_string)
